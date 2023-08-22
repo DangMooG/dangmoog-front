b821175f087a70120ee1a27e71f27f7b90be33c7
Kimcheolhui <cjfgml0306@naver.com>
2023-08-22 16:05:55 +0900

    fix: refactor the main appbar

---
+++ dangmoog/lib/screens/app_bar.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+AppBar mainAppBar(int currentTabIndex) {
+  switch (currentTabIndex) {
+    case 0:
+      return _postListAppbar();
+    case 1:
+      return _postListAppbar();
+    case 2:
+      return _chatListAppbar();
+    case 3:
+      return _myPageAppbar();
+    default:
+      return _postListAppbar();
+  }
+}
+
+AppBar _postListAppbar() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    leadingWidth: 200,
+    leading: Container(
+      padding: const EdgeInsets.only(left: 15, top: 13),
+      child: const Text(
+        'DOTORIT',
+        style: TextStyle(
+            color: Color(0xFFC30020),
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+    ),
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.notifications_none),
+        color: const Color(0xFFA07272),
+      ),
+    ],
+    bottom: PreferredSize(
+      preferredSize: const Size.fromHeight(0.0),
+      child: Container(
+        color: const Color(0xFFA07272),
+        height: 2.0,
+      ),
+    ),
+  );
+}
+
+AppBar _chatListAppbar() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    title: const Text(
+      "채팅 내역",
+      style: TextStyle(
+        color: Color(0xff552619),
+        fontSize: 19,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    bottom: PreferredSize(
+      preferredSize: const Size.fromHeight(0.0),
+      child: Container(
+        color: const Color(0xFFA07272),
+        height: 2.0,
+      ),
+    ),
+  );
+}
+
+AppBar _myPageAppbar() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    title: const Text(
+      "마이 도토릿",
+      style: TextStyle(
+        color: Color(0xff552619),
+        fontSize: 19,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    bottom: PreferredSize(
+      preferredSize: const Size.fromHeight(0.0),
+      child: Container(
+        color: const Color(0xFFA07272),
+        height: 2.0,
+      ),
+    ),
+  );
+}
--- dangmoog/lib/screens/home.dart
+++ dangmoog/lib/screens/home.dart
@@ -7,6 +7,8 @@ import 'package:dangmoog/screens/chat/chat_list.dart';
 import 'package:dangmoog/screens/post/main_page.dart';
 import 'package:dangmoog/screens/temp/temp_page.dart';
 
+import 'package:dangmoog/screens/app_bar.dart';
+
 class MyHome extends StatefulWidget {
   const MyHome({super.key});
 
@@ -27,7 +29,7 @@ class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: getAppBar(currentTabIndex),
+      appBar: mainAppBar(currentTabIndex),
       body: Center(
         child: _bodyPage.elementAt(currentTabIndex),
       ),
@@ -90,92 +92,4 @@ class _MyHomeState extends State<MyHome> {
       ),
     );
   }
-}
-
-AppBar getAppBar(int currentTabIndex) {
-  switch (currentTabIndex) {
-    case 0:
-      return _postListAppbar();
-    case 1:
-      return _postListAppbar();
-    case 2:
-      return _chatListAppbar();
-    case 3:
-      return _myPageAppbar();
-    default:
-      return _postListAppbar();
-  }
-}
-
-AppBar _postListAppbar() {
-  return AppBar(
-    backgroundColor: Colors.white,
-    leadingWidth: 200,
-    leading: Container(
-      padding: const EdgeInsets.only(left: 15, top: 13),
-      child: const Text(
-        'DOTORIT',
-        style: TextStyle(
-            color: Color(0xFFC30020),
-            fontSize: 24,
-            fontWeight: FontWeight.bold),
-      ),
-    ),
-    actions: [
-      IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.notifications_none),
-        color: const Color(0xFFA07272),
-      ),
-    ],
-    bottom: PreferredSize(
-      preferredSize: const Size.fromHeight(1.0),
-      child: Container(
-        color: const Color(0xFFA07272),
-        height: 2.0,
-      ),
-    ),
-  );
-}
-
-AppBar _chatListAppbar() {
-  return AppBar(
-    backgroundColor: Colors.white,
-    title: const Text(
-      "채팅 내역",
-      style: TextStyle(
-        color: Color(0xff552619),
-        fontSize: 19,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    bottom: PreferredSize(
-      preferredSize: const Size.fromHeight(1.0),
-      child: Container(
-        color: const Color(0xFFA07272),
-        height: 2.0,
-      ),
-    ),
-  );
-}
-
-AppBar _myPageAppbar() {
-  return AppBar(
-    backgroundColor: Colors.white,
-    title: const Text(
-      "마이 도토릿",
-      style: TextStyle(
-        color: Color(0xff552619),
-        fontSize: 19,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    bottom: PreferredSize(
-      preferredSize: const Size.fromHeight(1.0),
-      child: Container(
-        color: const Color(0xFFA07272),
-        height: 2.0,
-      ),
-    ),
-  );
 }
