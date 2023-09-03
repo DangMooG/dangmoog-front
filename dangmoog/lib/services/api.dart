628e4c52dbca4a22bb8e4afb0685ad5db262b2af
Kimcheolhui <cjfgml0306@naver.com>
2023-09-03 15:44:22 +0900

    add: added custom api module

---
+++ dangmoog/lib/services/api.dart
@@ -0,0 +1,63 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import 'package:dangmoog/services/custom_dio.dart';
+
+final dangmoogAPI = {
+  "emailSend": (email) async {
+    final api = dioAuthAPI();
+
+    try {
+      Response response = await api.post('/account/login', data: {
+        "email": email,
+      });
+      if (response.statusCode == 100) {
+        // 인증번호 입력
+      }
+    } catch (e) {
+      print(e);
+    }
+  },
+  "emailVerification": (verification_number) async {
+    final api = dioAuthAPI();
+    const storage = FlutterSecureStorage();
+
+    try {
+      Response response = await api.post('인증번호 전송 API 주소', data: {
+        "number": verification_number,
+      });
+      if (response.statusCode == 200) {
+        String refreshToken = response.data['refresh_token'];
+        String accessToken = response.data['access_token'];
+        String userId = response.data['user_id'];
+
+        await storage.write(
+          key: 'access_token',
+          value: accessToken,
+        );
+        await storage.write(
+          key: 'refresh_token',
+          value: refreshToken,
+        );
+        await storage.write(
+          key: 'user_id',
+          value: userId,
+        );
+      }
+    } catch (e) {
+      print(e);
+    }
+  },
+  "userNickname": (nickName) async {
+    final api = dioAPI();
+
+    try {
+      Response response = await api.post('닉네임 주소', data: {
+        "nick_name": nickName,
+      });
+      if (response.statusCode == 200) {}
+    } catch (e) {
+      print(e);
+    }
+  }
+};
