7932a9e47de02daa6dd2c441d12630e70bda2e5a
Kimcheolhui <cjfgml0306@naver.com>
2023-09-16 00:18:00 +0900

    fix: changed title to Dotorit

--- dangmoog/lib/main.dart
+++ dangmoog/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DangMooG',
+      title: 'Dotorit',
       debugShowCheckedModeBanner: false,
       theme: mainThemeData(),
       home: const SplashScreen(),
