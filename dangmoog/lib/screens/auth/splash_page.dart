e43466b174926501d739dbb4ef52867b3d564c5c
Kimcheolhui <cjfgml0306@naver.com>
2023-10-05 15:27:28 +0900

    add: added auto login process

--- dangmoog/lib/screens/auth/splash_page.dart
+++ dangmoog/lib/screens/auth/splash_page.dart
@@ -1,7 +1,13 @@
+import 'package:dangmoog/providers/provider.dart';
+import 'package:dangmoog/screens/auth/nickname.dart';
+import 'package:dangmoog/screens/home.dart';
+import 'package:dangmoog/services/api.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:dangmoog/screens/auth/welcome.dart';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,6 +17,16 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  static const storage = FlutterSecureStorage();
+
+  bool _isLoading = true;
+
+  void loadingDone() {
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -22,29 +38,59 @@ class _SplashScreenState extends State<SplashScreen> {
     });
   }
 
-  _navigateToHome() async {
-    await Future.delayed(const Duration(seconds: 3), () {});
+  _navigateToHome(BuildContext context) async {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const MyHome()),
+    );
+  }
+
+  _navigateToWelcome(BuildContext context) async {
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(builder: (context) => const WelcomePage()),
     );
   }
 
-  static const storage = FlutterSecureStorage();
+  _navigateToNickname(BuildContext context) async {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const NicknamePage()),
+    );
+  }
+
   autoLoginProcess() async {
     // access_token과 refresh_token이 있는지 확인
-    final accessToken = await storage.read(key: "access_token");
-    final refreshToken = await storage.read(key: "refresh_token");
+    final accessToken = await storage.read(key: 'accessToken');
+    final userId = await storage.read(key: 'userId');
 
-    if (refreshToken != null) {
-      // 서버로 유효한 토큰이 맞는지 확인 요청
+    if (accessToken != null && userId != null) {
+      try {
+        Response response = await ApiService().autoLogin();
 
-      // 유효할 경우 -> home으로 이동
-      // 만료된 토큰일 경우 -> 새로 받은 토큰을 기기에 저장하고 home으로 이동
-      // 잘못된 토큰일 경우 -> 로그인 페이지로 이동
+        // 유효한 토큰
+        if (response.statusCode == 200) {
+          final userEmail = response.data["email"] + "@gist.ac.kr";
+          final userNickname = response.data['username'];
+
+          Provider.of<UserProvider>(context, listen: false).setEmail(userEmail);
+
+          if (userNickname == null) {
+            _navigateToNickname(context);
+          } else {
+            Provider.of<UserProvider>(context, listen: false)
+                .setNickname(userNickname);
+
+            _navigateToHome(context);
+          }
+        }
+      } catch (e) {
+        print(e);
+        _navigateToWelcome(context);
+      }
     } else {
       // 로그인 페이지로 이동
-      _navigateToHome();
+      _navigateToWelcome(context);
     }
   }
 
@@ -53,14 +99,22 @@ class _SplashScreenState extends State<SplashScreen> {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
         body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      child: Stack(
+        alignment: Alignment.center,
         children: [
-          SizedBox(height: screenSize.height * 0.24),
-          Image.asset(
-            'assets/images/loading_logo.png',
-            width: screenSize.width * 0.57,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: screenSize.height * 0.24),
+              Image.asset(
+                'assets/images/loading_logo.png',
+                width: screenSize.width * 0.57,
+              ),
+            ],
           ),
+          _isLoading
+              ? const CircularProgressIndicator()
+              : const SizedBox.shrink()
         ],
       ),
     ));
