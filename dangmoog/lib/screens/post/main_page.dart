8cb826c1a04910cf727e498adf9593b1682e6c9b
Kimcheolhui <cjfgml0306@naver.com>
2024-01-03 00:27:23 +0900

    chore: removed useless import

--- dangmoog/lib/screens/post/main_page.dart
+++ dangmoog/lib/screens/post/main_page.dart
@@ -1,9 +1,5 @@
-// /screens/main_page.dart
 import 'package:flutter/material.dart';
 import 'package:dangmoog/screens/post/post_list.dart';
-import 'package:dangmoog/models/product_class.dart';
-import 'dart:convert';
-import 'package:flutter/services.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
