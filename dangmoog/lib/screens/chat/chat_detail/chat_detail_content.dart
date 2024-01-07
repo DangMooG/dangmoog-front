fb5ff8e270eb9a4ae1f07367d601bc6c1156b751
Kimcheolhui <cjfgml0306@naver.com>
2024-01-08 00:12:57 +0900

    fix: added cond scrolldownbtn visibility

--- dangmoog/lib/screens/chat/chat_detail/chat_detail_content.dart
+++ dangmoog/lib/screens/chat/chat_detail/chat_detail_content.dart
@@ -1,3 +1,4 @@
+import 'package:dangmoog/models/chat_detail_message_model.dart';
 import 'package:dangmoog/models/chat_detail_model.dart';
 import 'package:dangmoog/providers/chat_provider.dart';
 import 'package:dangmoog/screens/chat/chat_detail/chat_detail_cell.dart';
@@ -19,13 +20,51 @@ class ChatContents extends StatefulWidget {
 }
 
 class _ChatContentsState extends State<ChatContents> {
+  void scrollDown() {
+    widget.scrollController.animateTo(
+      widget.scrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeIn,
+    );
+  }
+
+  bool showScrollDownButton = false;
+
+  void _scrollListener() {
+    bool isNearBottom = widget.scrollController.position.maxScrollExtent -
+            widget.scrollController.offset <=
+        200;
+
+    if (isNearBottom && showScrollDownButton) {
+      setState(() {
+        showScrollDownButton = false;
+      });
+    } else if (!isNearBottom && !showScrollDownButton) {
+      setState(() {
+        showScrollDownButton = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    widget.scrollController.removeListener(_scrollListener);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Consumer<ChatProvider>(
         builder: (context, chatProvider, child) {
-          List<ChatDetailContent> chatContents = chatProvider.chatContents;
+          List<ChatDetailMessageModel> chatContents = chatProvider.chatContents;
 
           // 데이터가 비어 있으면 아무것도 반환하지 않음
           if (chatContents.isEmpty) {
@@ -45,71 +84,69 @@ class _ChatContentsState extends State<ChatContents> {
                 itemBuilder: (context, index) {
                   final singleChat = chatContents[index];
 
-                  var omit = false;
+                  var profileOmit = false;
                   var dateVisible = false;
 
                   // 맨 첫 채팅이 아니라면
                   if (index != 0) {
                     // 이전 채팅과 같은 유저이면서 같은 날짜이면
                     // -> 프로필 중복 표시 제거
-                    if ((chatContents[index - 1].isMe == singleChat.isMe) &&
-                        (isDifferentDate(chatContents[index - 1].chatDateTime,
-                                singleChat.chatDateTime) ==
-                            false)) {
-                      omit = true;
+                    if ((chatContents[index - 1].fromBuyer ==
+                            singleChat.fromBuyer) &&
+                        (isSameDate(chatContents[index - 1].createTime,
+                            singleChat.createTime))) {
+                      profileOmit = true;
                     } else {
                       // 이전 채팅과 다른 유저이면서 이전 채팅과 다른 날짜이면 -> 날짜 위젯 표시
-                      if (isDifferentDate(chatContents[index - 1].chatDateTime,
-                          singleChat.chatDateTime)) {
+                      if (!isSameDate(chatContents[index - 1].createTime,
+                          singleChat.createTime)) {
                         dateVisible = true;
                       }
                     }
                   } else {
                     dateVisible = true;
                   }
+
+                  // print(singleChat.fromBuyer);
+                  // print(chatProvider.imbuyer);
                   return Column(
                     children: [
                       dateVisible
-                          ? _buildChatDay(singleChat.chatDateTime)
+                          ? _buildChatDay(singleChat.createTime)
                           : const SizedBox.shrink(),
                       SingleChatMessage(
-                        text: singleChat.chatText,
-                        me: singleChat.isMe,
-                        omit: omit,
-                        time: convertTimeFormat(singleChat.chatDateTime),
+                        text: singleChat.message,
+                        me: singleChat.fromBuyer == chatProvider.imbuyer,
+                        profileOmit: profileOmit,
+                        time: convertTimeFormat(singleChat.createTime),
                       )
                     ],
                   );
                 },
               ),
-              Positioned(
-                right: 0,
-                bottom: 23,
-                child: GestureDetector(
-                  onTap: () {
-                    widget.scrollController.animateTo(
-                      widget.scrollController.position.maxScrollExtent,
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeOut,
-                    );
-                  },
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(25),
-                    child: Container(
-                        decoration:
-                            const BoxDecoration(color: Color(0xffF28C9D)),
-                        height: 48,
-                        width: 48,
-                        child: Transform.rotate(
-                          angle: -pi / 2,
-                          child: const Icon(
-                            Icons.keyboard_backspace,
-                            color: Colors.white,
-                          ),
-                        )),
+              if (showScrollDownButton)
+                Positioned(
+                  right: 0,
+                  bottom: 23,
+                  child: GestureDetector(
+                    onTap: scrollDown,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(25),
+                      child: Container(
+                          decoration:
+                              const BoxDecoration(color: Color(0xffF28C9D)),
+                          height: 48,
+                          width: 48,
+                          child: Transform.rotate(
+                            angle: -pi / 2,
+                            child: const Icon(
+                              Icons.keyboard_backspace,
+                              color: Colors.white,
+                            ),
+                          )),
+                    ),
                   ),
-                ),
-              )
+                )
             ],
           );
         },
@@ -139,8 +176,8 @@ Widget _buildChatDay(DateTime dateTime) {
 }
 
 // 다른 날짜 인지 확인
-bool isDifferentDate(DateTime date1, DateTime date2) {
-  return date1.day != date2.day ||
-      date1.month != date2.month ||
-      date1.year != date2.year;
+bool isSameDate(DateTime date1, DateTime date2) {
+  return date1.day == date2.day &&
+      date1.month == date2.month &&
+      date1.year == date2.year;
 }
