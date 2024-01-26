633bdb19ae92432e78b632a697dca8f813545d18
dwjustin <dwjustin@gm.gist.ac.kr>
2024-01-27 04:30:40 +0900

    feat: report 기능 추가

---
+++ dangmoog/lib/screens/report/post_report.dart
@@ -0,0 +1,39 @@
+import 'package:dangmoog/models/product_class.dart';
+import 'package:flutter/material.dart';
+
+// Assuming ProductModel is defined somewhere in your project
+
+class PostReportPage extends StatefulWidget {
+  final ProductModel product;
+
+  PostReportPage({Key? key, required this.product}) : super(key: key);
+
+  @override
+  _PostReportPageState createState() => _PostReportPageState();
+}
+
+class _PostReportPageState extends State<PostReportPage> {
+  // Add any state variables and methods here
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('게시글 신고'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Report for: ${widget.product.title}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            // Add more widgets here for the report functionality
+          ],
+        ),
+      ),
+    );
+  }
+}
